09537da085f1b8d4ec62e4a1fa8faaa80b5772dc
Eboshart <Erwinboshart@gmail.com>
2016-03-14 16:38:48 +0100

    Added 16

---
+++ src/nl/eboshart/demo/assignment16.java
@@ -0,0 +1,12 @@
+package nl.eboshart.demo;
+
+public class assignment16 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<21;i++) {
+			sum=sum+i;
+		}
+		System.out.println(sum);
+		System.out.println(20*21/2);
+	}
+}
