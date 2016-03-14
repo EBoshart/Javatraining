7670880ca470eac765a1d30341ab8b7ddd3db57e
Eboshart <Erwinboshart@gmail.com>
2016-03-14 16:29:24 +0100

    Added assignment14

---
+++ src/nl/eboshart/demo/assignment14.java
@@ -0,0 +1,14 @@
+package nl.eboshart.demo;
+
+public class assignment14 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<20;i=i+2) {
+			sum=sum+i;
+			
+		}
+		System.out.println(sum);
+		System.out.println(1+3+5+7+9+11+13+15+17+19);
+	}
+
+}
