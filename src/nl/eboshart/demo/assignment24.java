21e54d688a345123c457e0c4f3695e8ebe7675a2
Eboshart <Erwinboshart@gmail.com>
2016-03-15 16:52:46 +0100

    unfinished 24

---
+++ src/nl/eboshart/demo/assignment24.java
@@ -0,0 +1,15 @@
+package nl.eboshart.demo;
+import java.util.Arrays;
+import java.util.Scanner;
+public class assignment24 {
+	public static void main(String[] args) {
+		String[] items={"a", "b", "c", "d","e","f"};
+		int[] prices={1,2,3,4,5,6};
+		Scanner s=new Scanner(System.in);
+		String input=s.next();
+		int index=Arrays.asList(items).indexOf(input);
+		if 
+		System.out.println(prices[index]);
+		
+	}
+}
