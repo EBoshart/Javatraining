1f7f20ae16cd932edbbd4a39dbefe1db282397b1
Eboshart <Erwinboshart@gmail.com>
2016-03-16 16:57:14 +0100

    End of day

---
+++ src/nl/eboshart/demo/opdracht.java
@@ -0,0 +1,17 @@
+package nl.eboshart.demo;
+
+public class opdracht {
+	public static void main(String[] args) {
+	
+	}
+	public static int aantalbytes(int type) {
+		byte a=type;
+		short b=type;
+		int c=type;
+		long d=type;
+		char e=type;
+		boolean f=type;
+		float g=type;
+		double h=type;
+	}
+}
